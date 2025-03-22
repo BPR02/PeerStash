3b9526f4249702f03eeb08d28d2ad6b57b415050
BPR02 <dev@bpr02.com>
2025-03-22 00:31:32 -0700

    feat(shell): create adduser script

---
+++ peerstash_shell/scripts/adduser.sh
@@ -0,0 +1,38 @@
+#!/bin/sh
+
+ORIGINAL_ADDUSER="/usr/sbin/adduser"
+DB_PATH="/peerstash/config/users.db"
+
+# get username
+if [[ "$1" =~ [-] ]] || [ "$#" -ne 1 ]; then
+    echo "Modified version of adduser for peerstash"
+    echo "usage: adduser USERNAME (consisting of [0-9a-z_])" >&2
+    exit 1
+fi
+USERNAME=$1
+
+# verify username
+if [[ "$USERNAME" =~ [^a-z0-90-9_] ]]; then
+    echo "username must consist of [0-9a-z_]"
+    exit 1
+fi
+if [ ${#USERNAME} -le 2 ] || [ ${#USERNAME} -ge 32 ]; then
+    echo "username must be between 3 and 31 characters"
+    exit 1
+fi
+
+# create user
+$ORIGINAL_ADDUSER -D -h /peerstash/backups/$USERNAME "$USERNAME"
+
+# generate random password
+PASSWORD=$(head -c 16 /dev/urandom | base64 | tr -dc 'a-zA-Z0-9')
+
+# save hashed password into database
+PASSWORD_HASH=$(openssl passwd -6 $PASSWORD)
+sqlite3 "$DB_PATH" "INSERT INTO users (username, password_hash) VALUES ('$USERNAME','$PASSWORD_HASH');"
+
+# update password
+echo "$USERNAME:$PASSWORD_HASH" | chpasswd -e > /dev/null 2>&1
+
+# print username and password
+echo "$USERNAME:$PASSWORD"
