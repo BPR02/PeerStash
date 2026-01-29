4f256f5a33c93a3e8338bd3991247f96e54769d6
BPR02 <dev@bpr02.com>
2026-01-28 16:58:26 -0800

    retry script for debian

--- peerstash-control/scripts/setup.sh
+++ peerstash-control/scripts/setup.sh
@@ -3,21 +3,21 @@
 SSH_FOLDER="/var/lib/peerstash"
 
 # Generate SSH keys
+mkdir -p /var/run/sshd
+
 if [ ! -f "$SSH_FOLDER"/ssh_host_rsa_key ]; then
     echo "Generating SSH host keys..."
     ssh-keygen -A
-    mkdir -p "$SSH_FOLDER"
     cp /etc/ssh/ssh_host_* "$SSH_FOLDER"/
 else
     echo "Using existing SSH host keys..."
     cp "$SSH_FOLDER"/ssh_host_* /etc/ssh/
 fi
 
-# make a dummy user
-adduser "$USERNAME"
+# create admin user
+useradd -rm -d /home/$USERNAME -s /bin/bash -g root -G sudo -u 1000 $USERNAME
 echo "$USERNAME:$PASSWORD" | chpasswd
 
 # Start SSH server
 echo "Starting SSH service for remote machines..."
-# /usr/sbin/sshd -D
-systemctl start ssh
+/usr/sbin/sshd -D
