4909387ddd8b1a8388ffa011dc647eb73c1729d4
BPR02 <dev@bpr02.com>
2026-02-04 09:19:43 -0800

    fix known_hosts register

--- peerstash-control/scripts/register.sh
+++ peerstash-control/scripts/register.sh
@@ -15,8 +15,8 @@ fi
 # extract information from share key
 JSON_STRING=$(echo "$SHARE_KEY" | grep -o "[^#]*$" | openssl base64 -d -A)
 USERNAME=$(echo "$JSON_STRING" | jq -r ".username")
-SERVER_PUBLIC_KEY=$(echo "$JSON_STRING" | jq ".server_public_key")
-CLIENT_PUBLIC_KEY=$(echo "$JSON_STRING" | jq ".client_public_key")
+SERVER_PUBLIC_KEY=$(echo "$JSON_STRING" | jq -r ".server_public_key")
+CLIENT_PUBLIC_KEY=$(echo "$JSON_STRING" | jq -r ".client_public_key")
 
 # Set default quota
 if [ -z $QUOTA_GB ]; then
@@ -25,11 +25,6 @@ fi
 
 # Convert GB to Bytes
 QUOTA_BYTES=$(($QUOTA_GB * 1024 * 1024 * 1024))
-
-# add SFTP key to known hosts
-echo "" >> ~/.ssh/known_hosts
-echo "[peerstash-${USERNAME}]:2022 $SERVER_PUBLIC_KEY" >> ~/.ssh/known_hosts
-cp -p ~/.ssh/known_hosts $SSH_FOLDER/known_hosts
 
 # add user to SFTPGo
 if curl -s --fail --request POST \
@@ -40,12 +35,18 @@ if curl -s --fail --request POST \
     --data "{
     \"status\": 1,
     \"username\": \"$USERNAME\",
-    \"public_keys\": [$CLIENT_PUBLIC_KEY],
+    \"public_keys\": [\"$CLIENT_PUBLIC_KEY\"],
     \"quota_size\": $QUOTA_BYTES,
     \"permissions\": {\"/\":[\"*\"]}
 }" > /dev/null; then
-  echo "User $USERNAME created with ${QUOTA_GB}GB limit." >&2
-  exit 0
+    echo "User $USERNAME created with ${QUOTA_GB}GB limit." >&2
+
+    # add SFTP key to known hosts
+    echo "" >> ~/.ssh/known_hosts
+    echo "[peerstash-${USERNAME}]:2022 $SERVER_PUBLIC_KEY" >> ~/.ssh/known_hosts
+    cp -p ~/.ssh/known_hosts $SSH_FOLDER/known_hosts
+
+    exit 0
 fi
 # update SFTPGo user if already exists
 if curl -s --fail --request PUT \
@@ -56,13 +57,19 @@ if curl -s --fail --request PUT \
     --data "{
     \"status\": 1,
     \"username\": \"$USERNAME\",
-    \"public_keys\": [$CLIENT_PUBLIC_KEY],
+    \"public_keys\": [\"$CLIENT_PUBLIC_KEY\"],
     \"quota_size\": $QUOTA_BYTES,
     \"permissions\": {\"/\":[\"*\"]}
 }" > /dev/null; then
-  echo "User $USERNAME updated with ${QUOTA_GB}GB limit." >&2
-  exit 0
+    echo "User $USERNAME updated with ${QUOTA_GB}GB limit." >&2
+
+    # update SFTP key to known hosts
+    sed -i "/^\[peerstash-${USERNAME}\]*/c\
+        [peerstash-${USERNAME}]:2022 $SERVER_PUBLIC_KEY" ~/.ssh/known_hosts
+    cp -p ~/.ssh/known_hosts $SSH_FOLDER/known_hosts
+
+    exit 0
 else
-  echo "Failed to create user." >&2
-  exit 1
+    echo "Failed to create user." >&2
+    exit 1
 fi
