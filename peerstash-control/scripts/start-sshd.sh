a2804f8a2f293c50faccbf3eac585eb0d668c577
BPR02 <dev@bpr02.com>
2026-03-16 17:45:22 -0700

    timestamp process logs

---
+++ peerstash-control/scripts/start-sshd.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Peerstash
+# Copyright (C) 2026 BPR02
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+# timestamp both standard output and standard error and sent to stdout
+set -o pipefail
+/usr/sbin/sshd -D -e 2>&1 | awk '{ print strftime("[%Y-%m-%d %H:%M:%S]"), $0; fflush() }'
